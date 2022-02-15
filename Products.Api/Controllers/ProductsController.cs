ea526d6ce48527f19dfc92ca6f47ee2519c91d09
Luis Hernández Duarte <luis.hernandez@pevaar.com>
2022-02-15 08:59:46 -0500

    [ADD] First commit

---
+++ Products.Api/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Products.Infrastructure.Repositories;
+
+namespace Products.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var Product = new ProductRepository().GetProducts();
+            return Ok(Product);
+        }
+    }
+}
---
+++ Products.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Products.Core.Entities;
+
+namespace Products.Infrastructure.Repositories
+{
+    public class ProductRepository
+    {
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = Enumerable.Range(1, 10).Select(i => new Product
+            {
+                ProductId = i,
+                Name = $"Name {i}",
+                Description = $"Description {i}",
+                TypeId = i,
+                Price = i*10,
+                PurchaseDate = DateTime.Now,
+                Active = true,
+            });
+
+            return products;
+        }
+    }
+}
